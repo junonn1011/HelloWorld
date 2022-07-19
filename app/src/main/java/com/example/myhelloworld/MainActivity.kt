add07b6e867c1c64901760ae31fcc06b0b210e0e
anreh <anurehka@gmail.com>
2022-07-19 12:16:22 +0800

    commit 20220719 1215

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -1,36 +1,41 @@
 package com.example.myhelloworld
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.InputDevice
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     //val --> constant
     // var --> still can change the value/put value later
     lateinit var diceImg : ImageView
     lateinit var numberText : TextView
+    lateinit var editPlayerName :EditText
+    lateinit var playerNameTV :TextView
     //var diceImg :ImageView? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImg = findViewById(R.id.diceImage)
-        numberText = findViewById(R.id.numberText)
+        numberText = findViewById(R.id.headerText)
+        editPlayerName = findViewById(R.id.editPlayerNameET)
+        playerNameTV = findViewById(R.id.playerName)
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener{rollDice()}
     }
 
     private fun rollDice(){
         Toast.makeText( this,"Button Clicked",
-        Toast.LENGTH_SHORT).show()
+            Toast.LENGTH_SHORT).show()
 
         val randomNumber = (1..6).random() //RANGE
 
-        val numberText: TextView = findViewById(R.id.numberText)
+        val numberText: TextView = findViewById(R.id.headerText)
         numberText.text = randomNumber.toString()
         val diceImg : ImageView=findViewById(R.id.diceImage)
         val imgSrc = when (randomNumber){
@@ -44,5 +49,16 @@ class MainActivity : AppCompatActivity() {
         diceImg.setImageResource(imgSrc)
         Toast.makeText( this,randomNumber.toString(),
             Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updatePlayerName(view: View){
+        playerNameTV.text = editPlayerName.text
+
+        editPlayerName.text.clear()
+        editPlayerName.clearFocus()
+
+        //hide keyboard after update player name
+        //val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodService
+        //imm.hideSoftInputFormWindow(view.windowToken,0)
     }
 }
